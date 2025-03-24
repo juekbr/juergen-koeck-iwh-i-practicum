ca2f7c7711d4995a7e07d23de72e89f70b7f10ac
juek <juergen.koeck@brandreach.at>
2025-03-24 15:04:48 +0100

    understanding pug

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// get api token from .env file
+
+let PRIVATE_APP_ACCESS = process.env.API_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-app.get('/contacts', async (req, res) => {
+app.get('/', async (req, res) => {
     const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     }
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        const response = await axios.get(contacts, { headers });
+        const data = response.data.results;
+        // console.log(resp.data.results);
+        // res.render('contacts', { title: 'Contacts | HubSpot APIs', data }); 
+        res.json(data);
+        console.log('try');
     } catch (error) {
         console.error(error);
+        console.log('catch');
     }
 });
 
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+
+
+
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+
+
+
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 

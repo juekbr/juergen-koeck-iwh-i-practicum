96e45a22ec496f2cf34cf1723893d30b244bd31b
juek <juergen.koeck@brandreach.at>
2025-03-25 17:29:49 +0100

    form create obj successfully + cleanup

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// get api token from .env file
-
+// get api token from .env file =>
 let PRIVATE_APP_ACCESS = process.env.API_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
@@ -54,6 +53,7 @@ app.post('/update-cobj', async (req, res) => {
     // my properties are bike_name, brand, frame_size
     // my objecttype = 2-140603059
     // 
+    // we going to use req.body instead of req.query, because we use post
     const update = {
         properties: {
             "bike_name": req.body.bike_name,
@@ -61,12 +61,8 @@ app.post('/update-cobj', async (req, res) => {
             "frame_size": req.body.frame_size
         }
     }
-    // we going to use req.body instead of req.query, because we use post
-    // const bike_name = req.body.bike_name;
-    // const brand = req.body.brand;
-    // const frame_size = req.body.frame_size;
 
-    const createBike = `https://api.hubapi.com/crm/v3/objects/2-140603059`;
+    const createBike = "https://api.hubapi.com/crm/v3/objects/2-140603059";
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
@@ -75,7 +71,7 @@ app.post('/update-cobj', async (req, res) => {
     try {  
         // axios.patch(url, data, config)
         // axios.put(url, data, config)
-        await axios.put(createBike, update, { headers } );
+        await axios.post(createBike, update, { headers } );
         // back to last page in client =>
         res.redirect('back');
     } catch(err) {
@@ -83,52 +79,6 @@ app.post('/update-cobj', async (req, res) => {
     }
 
 });
-
-
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
-});
-*/
 
 
 // * Localhost
